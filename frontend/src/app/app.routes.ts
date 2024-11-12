6999c3bf0866e3b33b8c1a690ef7f1a3700f9f81
Vinicius Lemke <vinicius.lemke@hotmail.com>
2024-11-12 00:29:07 -0300

    Alterações realizadas: Incrementos da ferramenta de estilização Tailwind | Ajuste do campo de rotas Http

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TransacoesComponent } from './transacoes/transacoes.component'; // Certifique-se de que o caminho está correto
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'transacoes', pathMatch: 'full' }, // Redireciona para /transacoes por padrão
+  { path: 'transacoes', component: TransacoesComponent }, // Define a rota para o componente de transações
+  { path: '**', redirectTo: 'transacoes' }, // Redireciona rotas inválidas para transações
+];
