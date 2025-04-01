a0ae0c9c233a4c1003e21a018b7d28d592cc35cf
vinicius.batista <vinicius.batista@sulamerica.com.br>
2025-04-01 18:59:05 -0300

    Teste no CI/CD

--- frontend/src/app/transacoes.service.spec.ts
+++ frontend/src/app/transacoes.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransacaoService } from './transacoes.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('TransacoesService', () => {
   let service: TransacaoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [TransacaoService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(TransacaoService);
   });
 
